6b4cc76551a15c4be5d511d9919102cc48f5a46b
adityajha21 <Welc0meT0FI$>
2022-01-25 13:27:35 +0530

    first commit

---
+++ src/main/java/com/example/graphQL/GraphQlApplication.java
@@ -0,0 +1,14 @@
+package com.example.graphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class GraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlApplication.class, args);
+	}
+
+}
